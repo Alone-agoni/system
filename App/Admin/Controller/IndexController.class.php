07b7db088aa55eeae9cdb863485628572f9f47c2
e <827400818@qq.com>
2016-10-10 11:27:19 +0800

    新增-我的主页

--- App/Admin/Controller/IndexController.class.php
+++ App/Admin/Controller/IndexController.class.php
@@ -1,8 +1,13 @@
 <?php
 namespace Admin\Controller;
+
 use Think\Controller;
-class IndexController extends CommonController {
+
+class IndexController extends CommonController
+{
     public function index(){
+        $Row = system_info();
+        $this->assign('Row',$Row);
         $this->display();
     }
 }
